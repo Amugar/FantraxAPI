55ffe9c4e0eae22b82b8e4d2e375184d4699c781
Amugar <160679388+Amugar@users.noreply.github.com>
2026-04-17 15:08:26 -0700

    Update setup.py
    Add python 3.10 compatibility

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
